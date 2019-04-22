2d410a0a17f37d667efd78ce8ac0670afebbaa6c
unknown <160400105@stu.hit.edu.cn>
2019-04-22 21:39:03 +0800

    sidebar_updatee

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -10,18 +10,29 @@
                     <el-menu-item index="/Main"><i class="zhao-icon-third-icon_work_fill"></i>
                         &nbsp; 主页
                     </el-menu-item>
-                    <el-menu-item  index="/Main"><i class="zhao-icon-third-icon_search"></i> &nbsp;
-                        我的预约
-                    </el-menu-item>
-                    <el-menu-item  index="/showExam"><i class="zhao-icon-third-icon_search"></i>
-                        &nbsp; 我的考试发布
-                    </el-menu-item>
-                    <el-menu-item  index="/publishRes"><i class="zhao-icon-third-icon_calendar"></i>
-                        &nbsp; 发布预约
-                    </el-menu-item>
-                    <el-menu-item  index="/publishExam"><i class="zhao-icon-third-icon_calendar"></i>
-                        &nbsp; 发布考试安排
-                    </el-menu-item>
+
+                    <el-submenu index="1">
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>预约管理</span>
+                        </template>
+                        <el-menu-item-group>
+                            <el-menu-item index="/Main" ><i class="zhao-icon-third-icon_search"></i> &nbsp; 我的预约</el-menu-item>
+                            <el-menu-item index="/publishRes" ><i class="zhao-icon-third-icon_calendar"></i> &nbsp; 发布预约</el-menu-item>
+                        </el-menu-item-group>
+                    </el-submenu>
+
+                     <el-submenu index="2">
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>&nbsp;考试管理</span>
+                        </template>
+                        <el-menu-item-group>
+                            <el-menu-item index="/showExam" ><i class="zhao-icon-third-icon_task"></i> &nbsp; 我的考试发布</el-menu-item>
+                            <el-menu-item index="/publishExam" ><i class="zhao-icon-third-icon_search"></i> &nbsp; 发布考试安排</el-menu-item>
+                        </el-menu-item-group>
+                    </el-submenu>
+
                     <el-menu-item index="/personInfo"><i class="zhao-icon-third-icon_boss"></i>
                         &nbsp; 个人信息
                     </el-menu-item>
